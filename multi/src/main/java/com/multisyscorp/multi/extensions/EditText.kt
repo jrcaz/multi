e69d9f315c64b8c739a9ad9df66a77a6882bf89c
Rommel Azucena <rcazucena@multisyscorp.com>
2022-09-14 17:22:21 +0800

    Add extension for checking if edit text has value or not

--- multi/src/main/java/com/multisyscorp/multi/extensions/EditText.kt
+++ multi/src/main/java/com/multisyscorp/multi/extensions/EditText.kt
@@ -3,3 +3,5 @@ package com.multisyscorp.multi.extensions
 import android.widget.EditText
 
 fun EditText.stringValue() = this.text.toString()
+
+fun EditText.hasValue(): Boolean = stringValue().isNotEmpty() && stringValue().isNotBlank()
