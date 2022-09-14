8535d75df356a203cfed4fe8e4d462fa1f497b5a
Rommel Azucena <rcazucena@multisyscorp.com>
2022-09-14 16:49:25 +0800

    Add extension for checking if the string is valid JSON

--- multi/src/main/java/com/multisyscorp/multi/extensions/String.kt
+++ multi/src/main/java/com/multisyscorp/multi/extensions/String.kt
@@ -1,7 +1,28 @@
 package com.multisyscorp.multi.extensions
 
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
 fun String.hasValue(): Boolean {
     return this.isNotEmpty() && this.isNotBlank()
 }
 
 fun String.hasNoValue(): Boolean = !hasValue()
+
+fun String.isJSON(): Boolean {
+    try {
+        JSONObject(this)
+    } catch (e: JSONException) {
+        try {
+            JSONArray(this)
+        } catch (e: JSONException) {
+            return false
+        }
+    }
+
+    if (!this.contains("{") || !this.contains("}"))
+        return false
+
+    return true
+}
