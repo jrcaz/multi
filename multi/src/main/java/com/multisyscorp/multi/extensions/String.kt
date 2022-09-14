1bc06bbe0477a7f5a97cf901e14f2625a1151912
Rommel Azucena <rcazucena@multisyscorp.com>
2022-09-14 16:54:41 +0800

    Add an extension to check if the string is valid mobile number

--- multi/src/main/java/com/multisyscorp/multi/extensions/String.kt
+++ multi/src/main/java/com/multisyscorp/multi/extensions/String.kt
@@ -3,6 +3,8 @@ package com.multisyscorp.multi.extensions
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 fun String.hasValue(): Boolean {
     return this.isNotEmpty() && this.isNotBlank()
@@ -25,4 +27,12 @@ fun String.isJSON(): Boolean {
         return false
 
     return true
+}
+
+fun String.isValidMobileNumber(): Boolean {
+    val regEx = "^[0][0-9]{10}$"
+    val inputStr: CharSequence = this
+    val pattern: Pattern = Pattern.compile(regEx, Pattern.UNICODE_CASE)
+    val matcher: Matcher = pattern.matcher(inputStr)
+    return matcher.matches()
 }
